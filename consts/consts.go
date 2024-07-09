50b14021751b434dc741990112aa2a0813559b04
ELebiTImi <timiturner359@gmail.com>
2024-07-09 11:13:17 +0000

    HSDK

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "TimileyinHyperChain"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Timi"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "Tim"
 )
 
 var ID ids.ID
